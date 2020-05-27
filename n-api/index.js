1987df9cad0f4ded7e5307e7a74ab32d2f9d1c4d
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-05-27 09:58:39 +0200

    napi: add ceph init code

--- n-api/index.js
+++ n-api/index.js
@@ -2,5 +2,7 @@ const ceph_napi = require('./build/Release/cephnapi.node');
 
 console.log(`librados version: ${ceph_napi.version()}`);
 module.exports = ceph_napi;
-const username = "admin"
-ceph_napi.init(username);
+const username = "client.admin"
+const clustername = "ceph"
+ceph_napi.init(username, clustername);
+//ceph_napi.init(username);
--- n-api/src/ceph-napi.cc
+++ n-api/src/ceph-napi.cc
@@ -1,5 +1,5 @@
 #include <napi.h>
-#include <rados/librados.h>
+#include <rados/librados.hpp>
 #include <string>
 #include <iostream>
 #include "ceph-napi.h"
@@ -21,13 +21,55 @@ Napi::String version(const Napi::CallbackInfo& info) {
 }
 
 void init(const Napi::CallbackInfo& info) {
-  Napi::Value arg0 = info[0];
-  if (!arg0.IsString()) {
-    throw Napi::TypeError::New(info.Env(), "username argument must be of type string");
-  }
-  Napi::String username = arg0.As<Napi::String>();
   std::cout << "ceph-napi init..." << '\n';
-  std::cout << "username: " << username.Utf8Value() << '\n';
+  Napi::Env env = info.Env();
+  if (!info.Length() > 2) {
+    Napi::TypeError::New(env, "username and cluster arguments "
+        "must be specified").ThrowAsJavaScriptException();
+    return;
+  }
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "username argument must be specified")
+       .ThrowAsJavaScriptException();
+    return;
+  }
+  if (!info[1].IsString()) {
+    Napi::TypeError::New(env, "clustername argument must be specified")
+       .ThrowAsJavaScriptException();
+    return;
+  }
+
+  Napi::String user_name = info[0].As<Napi::String>();
+  Napi::String cluster_name = info[1].As<Napi::String>();
+  uint64_t flags = 0;
+  librados::Rados cluster;
+
+  std::cout << "user_name: " << user_name.Utf8Value() << ", cluster_name: " << cluster_name.Utf8Value() << '\n';
+  int ret = cluster.init2(user_name.Utf8Value().c_str(),
+                          cluster_name.Utf8Value().c_str(),
+                          flags);
+  if (ret < 0) {
+    Napi::TypeError::New(env, "Cound not initialize cluster handle")
+     .ThrowAsJavaScriptException();
+    return;
+  }
+
+  std::cout << "Created a cluster handle." << '\n';
+  ret = cluster.conf_read_file("/etc/ceph/ceph.conf");
+  if (ret < 0) {
+    Napi::TypeError::New(env, "Cound not read /etc/ceph/ceph.conf")
+     .ThrowAsJavaScriptException();
+    return;
+  }
+
+  ret = cluster.connect();
+  if (ret < 0) {
+    std::cout << "Connected not connect to the cluster. ret= " << ret << '\n';
+    Napi::TypeError::New(env, "Cound not read connect to the cluster")
+     .ThrowAsJavaScriptException();
+    return;
+  }
+  std::cout << "Connected to the cluster..." << '\n';
 }
 
 } // namespace cephnapi
