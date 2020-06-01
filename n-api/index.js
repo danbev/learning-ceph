28103d83196afa6ec4fbca183258c8aab0d81c6b
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-06-01 13:20:50 +0200

    napi: add buildType to module

--- n-api/index.js
+++ n-api/index.js
@@ -1,4 +1,7 @@
-const ceph_napi = require('./build/Release/cephnapi.node');
+const buildType = process.config.target_defaults ?
+  process.config.target_defaults.default_configuration :
+  'Release';
+const ceph_napi = require(`./build/${buildType}/cephnapi.node`);
 
 console.log(`librados version: ${ceph_napi.version()}`);
 module.exports = ceph_napi;
